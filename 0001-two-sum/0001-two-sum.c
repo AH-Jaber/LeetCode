e7e195815b336ec2fc262832e860aa9283ad2a9f
AH-Jaber <ahjaber10020@gmail.com>
2026-08-12 00:43:45 +0600

    Time: 35 ms (87.42%), Space: 12.4 MB (5.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -1,15 +1,45 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    for (int i = 0; i < numsSize; i++) {
-        for (int j = i + 1; j < numsSize; j++) {
-            if (nums[j] == target - nums[i]) {
-                int* result = malloc(sizeof(int) * 2);
-                result[0] = i;
-                result[1] = j;
-                *returnSize = 2;
-                return result;
+    int* a = calloc(2000000001, sizeof(int));
+    int x, y;
+    int i;
+    for (i = 0; i < numsSize; i++) {
+        a[nums[i] + 1000000000]++;
+    }
+
+    x = -1;
+    y = -1;
+    for (i = 0; i < numsSize; i++) {
+        int need = target - nums[i];
+
+        if (need >= -1000000000 && need <= 1000000000 && a[need + 1000000000] > 0) {
+            if (need != nums[i]) {
+                x = nums[i];
+                y = need;
+                break;
+            }
+            else if (a[need + 1000000000] > 1) {
+                x = nums[i];
+                y = need;
+                break;
             }
         }
     }
-    *returnSize = 0;
-    return malloc(sizeof(int) * 0);
+    int* ans = malloc(2 * sizeof(int));
+    *returnSize = 2;
+    ans[0] = -1;
+    ans[1] = -1;
+    
+    for (i = 0; i < numsSize; i++) {
+        if (nums[i] == x && ans[0] == -1)
+            ans[0] = i;
+        else if (nums[i] == y && ans[1] == -1)
+            ans[1] = i;
+    }
+    free(a);
+
+    return ans;
 }
